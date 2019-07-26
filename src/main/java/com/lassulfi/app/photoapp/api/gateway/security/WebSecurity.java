7173df3afbba7c1670af791d195ed9ec232ff6db
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-07-25 22:09:15 -0300

    Adding Spring Boot Actuator to API Gateway

--- src/main/java/com/lassulfi/app/photoapp/api/gateway/security/WebSecurity.java
+++ src/main/java/com/lassulfi/app/photoapp/api/gateway/security/WebSecurity.java
@@ -21,13 +21,12 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 		this.environment = environment;
 	}
 
-
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.headers().frameOptions().disable();
 		http.authorizeRequests()
+			.antMatchers(environment.getProperty("api.zuul.actuator.url.path")).permitAll()
 			.antMatchers(environment.getProperty("api.h2console.url.path")).permitAll()
 			.antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url.path")).permitAll()
 			.antMatchers(HttpMethod.POST, environment.getProperty("api.login.url.path")).permitAll()
